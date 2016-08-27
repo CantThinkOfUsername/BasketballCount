047daf02b1080e6f71e165c854a341b436776d95
cantthinkofusername <alexander00154@gmail.com>
2016-08-27 12:08:56 +0200

    removed one uneccessary line of code

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.android.courtcounter;
 
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
